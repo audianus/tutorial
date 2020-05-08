2508ef1c0865b7b59beeabc039f307c70af072ff
audianus <64883650+audianus@users.noreply.github.com>
2020-05-08 20:11:56 +0700

    Add files via upload

---
+++ Latihan 5.php
@@ -0,0 +1,16 @@
+<htmL>
+<head>
+	<title> Contoh Form Dengan POST</title>
+</head>
+<body>
+<form action+"latihan6.php" method="post">
+<h1> only for authorized user </h1>
+<hr>
+Username : <input type="text" name="username">
+<br>
+Password : <input type="Password" name="password">
+<br>
+<input type="submit" value="login">
+</form>
+</body>
+</html>
